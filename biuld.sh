abf5be84b772d665048c431c662f43e99f9615ca
Anna Zaitseva <a.z.zaitseva.anna@gmail.com>
2026-01-26 17:01:43 +0300

    Build.sh update

--- biuld.sh
+++ biuld.sh
@@ -1,5 +1,7 @@
 #Docker for Power Shell
 docker build -t dog-weight-tracker:v1.0.0 .
+or 
+docker build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t dog-weight-tracker:v1.0.0 .
  $imageId = docker images -q  dog-weight-tracker:v1.0.0
 
 docker image ls
